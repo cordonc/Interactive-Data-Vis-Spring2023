3b6836305a701962141e2254dad96c233633ccbb
cordonc <89549006+cordonc@users.noreply.github.com>
2023-03-21 15:58:20 -0400

    changes to basic html page

--- 2_1_quantities_and_amounts/main.js
+++ 2_1_quantities_and_amounts/main.js
@@ -4,15 +4,57 @@
 // const height = ;
 
 /* LOAD DATA */
-d3.csv('../[PATH_TO_YOUR_DATA]', d3.autoType)
+d3.csv('roster.csv', d3.autoType)
   .then(data => {
+    //parsing data -> changing csv to an array
     console.log("data", data)
 
     /* SCALES */
     /** This is where you should define your scales from data to pixel space */
     
-
+   
     /* HTML ELEMENTS */
     /** Select your container and append the visual elements to it */
+
+     //select container
+     const table = d3.select("#container")
+     .append("table")
+
+     table.append("thead")
+
+     table.append("tbody")
+       .append("tr")
+       .attr('class', 'row')
+
+     console.log('container', container)
+
+     // append table
+     // append table head
+     // append table body
+
+     // use d3 to populate table
+     //tbody.selectAll(".student") specifically elements to the student class
+
+     tbody.selectAll("tr")
+      .data(roster)
+      .join("tr")
+      .attr("class", "student")
+      // assign id
+      .attr("id", data => data.Last) // can't just write data.Last, needs to be a function.
+      .append("td") //can now manipulate table data to each cell
+      .text(data => data.Last)
+
+      //break the chain
+      // ad cell for first name to row
+      row.append("td")
+        .text(data => data.First)
+
+        // add cell for the last name to row
+        row
+          .append("td")
+          .text(data => data.Last)
+      
+
+
 
   })
