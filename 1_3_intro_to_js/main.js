46469dc33982de1f789c260ffe5434fd52dcc678
cordonc <89549006+cordonc@users.noreply.github.com>
2023-03-21 18:11:11 -0400

    exercise 3 javascript page

--- 1_3_intro_to_js/main.js
+++ 1_3_intro_to_js/main.js
@@ -1,1 +1,6 @@
-console.log('hello world');
+var button = document.getElementById("clickme"),
+  count = 0;
+button.onclick = function() {
+  count += 1;
+  button.innerHTML = "today i hit my juul: " + count;
+};
