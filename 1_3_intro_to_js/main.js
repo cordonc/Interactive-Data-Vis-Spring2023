80e6d8c2b8dd389a6b5284bddd6b0a8812870402
cordonc <89549006+cordonc@users.noreply.github.com>
2023-03-21 22:29:31 -0400

    typo in javascript

--- 1_3_intro_to_js/main.js
+++ 1_3_intro_to_js/main.js
@@ -2,5 +2,5 @@ var button = document.getElementById("clickme"),
   count = 0;
 button.onclick = function() {
   count += 1;
-  button.innerHTML = "today i hit my juul: " + count;
+  button.innerHTML = "today I hit my juul: " + count;
 };
